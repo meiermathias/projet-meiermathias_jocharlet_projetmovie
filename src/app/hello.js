c51efe7da6ff7a6817e6e9ebbfb8023bcea13b50
JoCharlet <jonathan.charlet@he-arc.ch>
2016-06-27 13:04:56 +0200

    removed capitalized function from hello.js

--- src/app/hello.js
+++ src/app/hello.js
@@ -6,9 +6,5 @@ angular
         //Bonne pratique redefinir : var $ctrl = this
       var $ctrl = this;
       $ctrl.maVariable = "Hello";
-
-      $ctrl.capitalize = function(text){
-          return text[0].toUpperCase() + text.slice(1).toLowerCase()
-      }
     }
   });
