0063dbaa09d084e4f58e15dfda392286f85698a2
JoCharlet <jonathan.charlet@he-arc.ch>
2016-06-27 14:00:28 +0200

    Added supprimerAListe function

--- src/app/hello.js
+++ src/app/hello.js
@@ -2,14 +2,27 @@ angular
   .module('app')
   .component('app', {
     templateUrl: 'app/hello.html',
-    controller: function (){
-        //Bonne pratique redefinir : var $ctrl = this
+    controller: function() {
+      //Bonne pratique redefinir : var $ctrl = this
       var $ctrl = this;
-      $ctrl.maListe=['hello','bonjour'];
+      $ctrl.maListe = ["District 9", "Matrix"];
       $ctrl.maVariable = "Hello";
 
-      $ctrl.ajouterAListe = function () {
-          $ctrl.maListe.push("au revoir");
+      $ctrl.ajouterAListe = function() {
+        $ctrl.maListe.push($ctrl.maVariable);
       };
+
+  /*    $ctrl.suprimmerAListe = function($index) {
+        $ctrl.maListe.splice($index,1);
+      };
+*/
+$ctrl.suprimmerAListe = function(el) {
+     $ctrl.maListe.splice($ctrl.maListe.indexOf(el),1);
+   };
+
     }
   });
+
+
+// indexOf():
+//splice(index,1);
