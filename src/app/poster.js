43c8ed0b82a1c9d0887b1654d10fd0992efc7d03
JoCharlet <jonathan.charlet@he-arc.ch>
2016-06-28 18:53:44 +0200

    Added poster code

--- src/app/poster.js
+++ src/app/poster.js
@@ -1,11 +1,11 @@
-function posterController() {
-  this.text = 'My brand new component!';
-}
 
 angular
   .module('app')
   .component('poster', {
     templateUrl: 'app/poster.html',
-    controller: posterController
+    controller: function posterControleler() {
+    },
+    bindings: {
+      movie: '<'
+    }
   });
-
