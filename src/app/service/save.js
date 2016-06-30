ddef43c33d2e64566bfc049eb181360c5f7b9c3c
JoCharlet <jonathan.charlet@he-arc.ch>
2016-06-30 13:43:03 +0200

    Added favorit code to handle removed and add to favorit

--- src/app/service/save.js
+++ src/app/service/save.js
@@ -2,16 +2,27 @@ angular
     .module('app')
     .service('Save', function () {
       var Save = this;
+
       Save.save = function (toBeSaved) {
         var toBeStored = JSON.stringify(toBeSaved);
-        // var idStored = JSON.stringify(toBeSaved.id);
         localStorage.setItem('saved', toBeStored);
       };
+
       Save.load = function () {
         var unString = localStorage.getItem('saved');
         if (unString) {
           return JSON.parse(unString);
         }
         return [];
+      };
+
+      Save.remove = function (toBeRemoved) {
+        Save.liste = Save.load();
+        for (var i = 0; i < Save.liste.length; i++) {
+          if (Save.liste[i].id === toBeRemoved.id) {
+            Save.liste.splice(i, 1);
+          }
+        }
+        Save.save(Save.liste);
       };
     });
