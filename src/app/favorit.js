e9d489e00b2860c3d581ed346628f509d1ba016f
JoCharlet <jonathan.charlet@he-arc.ch>
2016-06-30 09:44:05 +0200

    Added favorit function

--- src/app/favorit.js
+++ src/app/favorit.js
@@ -1,11 +1,21 @@
-function favoritController() {
-  this.text = 'My brand new component!';
-}
-
 angular
   .module('app')
   .component('favorit', {
     templateUrl: 'app/favorit.html',
-    controller: favoritController
-  });
+    controller: function favoritController(Save, load, $http) {
+      var $ctrl = this;
+      $http.get('https://amc.ig.he-arc.ch/tmdb/movie/upcoming?language=fr')
+      .then(function (result) {
+        $ctrl.movies = result.data.results;
+      });
+      $ctrl.maListe = load.load();
 
+      $ctrl.suprimmerAListe = function ($index) {
+        $ctrl.maListe.splice($index, 1);
+      };
+
+      $ctrl.suprimmerAListe = function (el) {
+        $ctrl.maListe.splice($ctrl.maListe.indexOf(el), 1);
+      };
+    }
+  });
--- src/app/service/save.js
+++ src/app/service/save.js
@@ -1,18 +1,16 @@
-function Save() {
-  var Save = this;
-  Save.save = function (toBeSaved) {
-    var toBeStored = JSON.stringify(toBeSaved);
-    localStorage.setItem('saved', toBeStored);
-  };
-  Save.load = function () {
-    var unString = localStorage.getItem('saved');
-    if (unString) {
-      return JSON.parse(unString);
-    }
-    return [];
-  };
-}
-
 angular
     .module('app')
-    .service('Save', Save);
+    .service('Save', function () {
+      var Save = this;
+      Save.save = function (toBeSaved) {
+        var toBeStored = JSON.stringify(toBeSaved);
+        localStorage.setItem(toBeSaved.id, toBeStored);
+      };
+      Save.load = function () {
+        var unString = localStorage.getItem('saved');
+        if (unString) {
+          return JSON.parse(unString);
+        }
+        return [];
+      };
+    });
